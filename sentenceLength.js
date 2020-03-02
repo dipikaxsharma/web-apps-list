5e0f7411ea6040970ada294ee59f9a6bc1a73c4c
dipikaxsharma <sharmadipika55@hotmail.com>
2020-03-02 10:47:44 -0600

    incomplete devC and sentence length

---
+++ sentenceLength.js
@@ -0,0 +1,10 @@
+
+<script>
+function sentenceLength() {
+    
+} sentenceLength() {
+  var str = document.getElementById("sentenceLength").value;;
+  var n = str.length;
+  document.getElementById("demo").devC2.HTML = n;
+}
+</script>
